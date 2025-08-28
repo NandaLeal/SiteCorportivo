ecbfe7f86f72294271752cf90e82609c5b400573
Gabriel Pinheiro Saraiva <159718367+GabrielPSaraiva@users.noreply.github.com>
2025-08-28 13:56:50 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,7 +25,7 @@ const footerNavigation = {
     },
     {
       name: "WhatsApp",
-      href: "https://wa.me/553832131234",
+      href: "https://wa.me/553832131234?text=Seja bem vindo a maior revenda do Norte de Minas.",
       icon: MessageCircle,
     },
     {
