1b448b948f5ef725ca5af56dcd669dea92408884
Fernanda Pimenta Leal <86990330+NandaLeal@users.noreply.github.com>
2025-08-28 16:05:17 -0300

    Update Contato.tsx

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -200,18 +200,20 @@ export default function Contato() {
                 Estamos localizados em Montes Claros, atendendo todo o Norte de Minas
               </p>
             </div>
-            
             <Card className="shadow-elegant overflow-hidden">
-              <div className="aspect-[16/9] bg-muted flex items-center justify-center">
-                <div className="text-center">
-                  <MapPin className="h-12 w-12 text-primary mx-auto mb-4" />
-                  <p className="text-lg font-semibold">Mapa Interativo</p>
-                  <p className="text-muted-foreground">
-                    Montes Claros - Norte de Minas Gerais
-                  </p>
-                </div>
+                <div className="aspect-[16/9]">
+                  <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1549.9183167952733!2d-43.80456646070933!3d-16.68381655431447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x754aa6c5384419b%3A0xe5316dc8a4e1aa5d!2sDistribuidora%20de%20Bebidas%20Cervantes!5e0!3m2!1spt-BR!2sbr!4v1756407698944!5m2!1spt-BR!2sbr" 
+                    width="600" 
+                    height="450" 
+                    style="border:0;" 
+                    allowfullscreen="" 
+                    loading="lazy" r
+                    eferrerpolicy="no-referrer-when-downgrade"
+                    ></iframe>
               </div>
-            </Card>
+          </Card>
+
           </div>
         </section>
       </main>
