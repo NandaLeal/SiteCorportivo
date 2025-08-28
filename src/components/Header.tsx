b82153b5ec1920e7b5dc20e4c96e0737382ba00b
Fernanda Pimenta Leal <86990330+NandaLeal@users.noreply.github.com>
2025-08-28 14:37:38 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,9 +22,13 @@ export default function Header() {
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <Link to="/" className="-m-1.5 p-1.5">
-            <span className="text-2xl font-bold bg-gradient-primary bg-clip-text text-transparent">
-              Cervantes
-            </span>
+            <Link to="/" className="-m-1.5 p-1.5">
+  <img
+    src="/LOGO.jpg"
+    alt="Cervantes Distribuidora"
+    className="h-12 w-auto"
+  />
+</Link>
           </Link>
         </div>
         <div className="flex lg:hidden">
