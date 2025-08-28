9e8383640242f30d6ae13e17e9d0614494df07d6
Gabriel Pinheiro Saraiva <159718367+GabrielPSaraiva@users.noreply.github.com>
2025-08-28 13:50:55 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,7 +25,7 @@ const footerNavigation = {
     },
     {
       name: "WhatsApp",
-      href: "https://api.whatsapp.com/send/?phone=553832151234&text&type=phone_number&app_absent=0",
+      href: "https://api.whatsapp.com/send/?phone=5538999999999&text&type=phone_number&app_absent=0",
       icon: MessageCircle,
     },
     {
