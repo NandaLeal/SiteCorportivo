54da3f1c17589f3b8c6ba142f6d328b19d658b0c
Gabriel Pinheiro Saraiva <159718367+GabrielPSaraiva@users.noreply.github.com>
2025-08-29 13:48:57 -0300

    Update LogoProcessor.tsx

--- src/components/LogoProcessor.tsx
+++ src/components/LogoProcessor.tsx
@@ -34,7 +34,7 @@ export const LogoProcessor = ({ mobile = false }: LogoProcessorProps) => {
     <img
       src={processedLogoUrl || logoImage}
       alt="Cervantes Distribuidora"
-     <div className={"h-16 w-40" }> {height 64px width 180px; }
+     <div className={"h-16 w-40" }> 
       <LogoProcessor/>
       </div>
   </img>
