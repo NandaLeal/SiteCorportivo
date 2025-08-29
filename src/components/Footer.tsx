84d5e874255e35b3e6c1b0b558f9a96b29d20dd2
Gabriel Pinheiro Saraiva <159718367+GabrielPSaraiva@users.noreply.github.com>
2025-08-29 13:54:22 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -40,7 +40,7 @@ const footerNavigation = {
 export default function Footer() {
   return (
     <footer className="bg-gradient-elegant border-t">
-      <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
+      <div className="mx-auto max-w-7xl px-6 pb-8 pt-14 sm:pt-24 lg:px-8 lg:pt-32">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
           <div className="space-y-8">
             <Link to="/" className="text-2xl font-bold bg-gradient-primary bg-clip-text text-transparent">
