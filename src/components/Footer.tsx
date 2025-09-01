6a8ddd1c5bd1973136b81c06973d701ff1229a0a
Gabriel Pinheiro Saraiva <159718367+GabrielPSaraiva@users.noreply.github.com>
2025-09-01 13:04:58 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -47,7 +47,7 @@ export default function Footer() {
               <img 
                 src={logoFundo}
                 alt="Cervantes Distribuidora"
-                className="h-25 w-82 object-contain"
+                className="h-22 w-76 object-contain"
               />
             </Link>
             <p className="text-sm leading-6 text-muted-foreground">
