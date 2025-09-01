76f3a09c9e7d0789e14a6d292491c16529d2e663
Gabriel Pinheiro Saraiva <159718367+GabrielPSaraiva@users.noreply.github.com>
2025-09-01 12:34:31 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -81,7 +81,7 @@ export default function Header() {
 
   return (
     <header className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b border-border shadow-soft">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between py-2 px-3 lg:px-6" aria-label="Global">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between py-2 px-3 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1 -ml-2">
           <Link to="/" className="-m-1.5 p-1.5">
             <div className="h-16 w-32">
