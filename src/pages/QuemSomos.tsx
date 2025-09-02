b48debedef2d04bbbd0d08c5e29121ee5723e843
Gabriel Pinheiro Saraiva <159718367+GabrielPSaraiva@users.noreply.github.com>
2025-09-02 11:21:04 -0300

    Update QuemSomos.tsx

--- src/pages/QuemSomos.tsx
+++ src/pages/QuemSomos.tsx
@@ -18,7 +18,7 @@ export default function QuemSomos() {
                 Quem Somos
               </h1>
               <p className="text-lg leading-8 text-muted-foreground">
-                +30 anos de tradição, inovação e compromisso com a excelência no Norte de Minas
+                A mais de 30 anos de tradição, inovação e compromisso com a excelência no Norte de Minas
               </p>
             </div>
           </div>
